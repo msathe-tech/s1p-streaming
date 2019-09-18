7f7d54cfa92c003018f334ff23c307a19b8c2821
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-09-18 10:44:07 -0400

    start kafka just-ride

---
+++ kafka/kafka-just-ride-i1/src/main/java/com/example/kafkajustridei1/KafkaJustRideI1Application.java
@@ -0,0 +1,13 @@
+package com.example.kafkajustridei1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJustRideI1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJustRideI1Application.class, args);
+	}
+
+}
---
+++ kafka/kafka-just-ride-i1/src/test/java/com/example/kafkajustridei1/KafkaJustRideI1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.kafkajustridei1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaJustRideI1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
