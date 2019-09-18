2e04fbbe6db26e30f8053d57d35c561e9b75c659
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-09-18 11:03:43 -0400

    PodEvent pojo

---
+++ kafka/kafka-just-ride-i1/src/main/java/com/example/kafkajustridei1/domain/PodEvent.java
@@ -0,0 +1,64 @@
+package com.example.kafkajustridei1.domain;
+
+import java.io.Serializable;
+
+public class PodEvent implements Serializable {
+
+	public PodEvent(String uuid, Double latitude, Double longitude, Double speed) {
+		this.uuid = uuid;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.speed = speed;
+	}
+
+	public PodEvent() {
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public void setSpeed(Double speed) {
+		this.speed = speed;
+	}
+
+	@Override
+	public String toString() {
+		return "PodEvent{" +
+				"uuid=" + uuid +
+				", latitude=" + latitude +
+				", longitude=" + longitude +
+				", speed=" + speed +
+				'}';
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public Double getSpeed() {
+		return speed;
+	}
+
+	private String uuid;
+	private Double latitude;
+	private Double longitude;
+	private Double speed;
+
+}
